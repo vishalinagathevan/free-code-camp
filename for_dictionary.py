edc0e98ff6b9f55591e1e60e6fd4f0eed491b95b
vishalinagathevan <126974938+vishalinagathevan@users.noreply.github.com>
2023-12-02 15:21:24 +0530

    Using for loop dictionary mothed

---
+++ for_dictionary.py
@@ -0,0 +1,18 @@
+dictionary = { "vishali": "M.sc IT" }
+
+for key,value in dictionary.items():
+    print("%s --> %s" %(key,value))# dictionary[value]))
+    
+# some_key --> some_value
+
+dictionary_tk = {
+  "name": "vishali",
+  "nickname": "vichu",
+  "nationality": "india",
+  "age": 23
+}
+
+for attribute, value in dictionary_tk.items():
+    print("My %s is %s " %(attribute, value))
+    
+# attribute is a dictionary key prameter 
