4638959029d5e228957e0c83ad90c016c850791a
vishalinagathevan <126974938+vishalinagathevan@users.noreply.github.com>
2023-12-18 10:27:25 +0530

    Added nested for loop & zip()

--- For_if.py
+++ For_if.py
@@ -28,3 +28,40 @@ while True:
 	i += 1
 
 print("Out of while loop ") 
+
+# Range method 
+
+# Python Program to
+# show range() basics
+
+# printing a number
+for i in range(10):
+	print(i, end=" ")
+
+# performing sum of first 10 numbers
+sum = 0
+for i in range(1, 10):
+	sum = sum + i
+print("\nSum of first 10 numbers :", sum)
+
+# OutPut is : 45 the workflow is below
+# Sum+ Range = total
+# 0 + 1 = 1
+# 1 + 2 = 3
+# 3 + 3 = 6
+# 6 + 4 = 10
+# 10 + 5 = 15
+# 15 + 6 = 21
+# 21 + 7 = 28
+# 28 + 8 = 36
+# 36 + 9 = 45
+
+
+
+
+
+
+
+
+
+
